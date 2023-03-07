bc69b5dfa22def44f52d98fa24e8d08cfba8a28d
Elias <sjoberg.sjoberg@elev.ga.ntig.se>
2023-03-07 14:22:12 +0100

    hej

--- src/Bakgrund.java
+++ src/Bakgrund.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class Bakgrund {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Bakgrund extends JPanel implements MouseListener {
+
+    //attrib
+    //costr
+    final int SIZE = 400;
+    private boolean firstcolor = false;
+    private boolean LR = false;
+    public Bakgrund(){
+
+        this.setBackground(Color.white);
+        this.setPreferredSize(new Dimension(SIZE,SIZE));
+        this.addMouseListener(this);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (LR) {
+            g.drawOval(20, 20, 350, 350);
+        } else {
+            g.drawLine(0, 0, SIZE, SIZE);
+            g.drawLine(0, SIZE, SIZE, 0);
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("YOU HAVE CLICKED!");
+        if (!firstcolor){
+            this.setBackground(Color.MAGENTA);
+        }else {
+            this.setBackground(Color.BLUE);
+        }
+        firstcolor=!firstcolor;
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            LR = false;
+        }
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            LR = true;
+        }
+        repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    //method
+
+    //getset
+
 }
--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    //attrib
+    //costr
+    public Window() {
+        this.setTitle("A Clicker");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.add(new Bakgrund());
+
+        this.pack();
+        this.setVisible(true);
+
+
+        //creat a layout
+
+        JPanel multiPanel = new JPanel();
+        GridLayout layout = new GridLayout(2, 2);
+        multiPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 10));
+        multiPanel.setLayout(layout);
+
+        multiPanel.add(new Bakgrund());
+        multiPanel.add(new Bakgrund());
+        multiPanel.add(new Bakgrund());
+        multiPanel.add(new Bakgrund());
+
+        this.getContentPane().add(multiPanel);
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+    //method
+
+    //getset
 }
